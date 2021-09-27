93deaea38a307abba14a0080717b0168e4e0a687
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-27 20:56:02 +0700

    Create the project

---
+++ GetUserLocation/GetUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUserLocationApp.swift
+//  GetUserLocation
+//
+//  Created by Danh Tu on 27/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct GetUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
