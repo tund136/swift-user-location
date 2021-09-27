ee84ae74881064a4d65bf4d24f11997a82a53751
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-27 21:06:27 +0700

    Building Map UI

--- GetUserLocation/ContentView.swift
+++ GetUserLocation/ContentView.swift
@@ -5,12 +5,20 @@
 //  Created by Danh Tu on 27/09/2021.
 //
 
+import CoreLocationUI
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40, longitude: 120), span: MKCoordinateSpan(latitudeDelta: 100, longitudeDelta: 100))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Map(coordinateRegion: $region, showsUserLocation: true)
+                .ignoresSafeArea()
+                .tint(.pink)
+            
+        }
     }
 }
 
